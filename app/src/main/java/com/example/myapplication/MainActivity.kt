48fdaa4163ddcff01c7d95b3446915b30a493e09
Moon Kyung Song <moonsong98@postech.ac.kr>
2020-12-30 17:35:55 +0900

    Implement Initial Tab

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TableLayout
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    lateinit var viewPager: ViewPager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tabLayout: TabLayout = findViewById(R.id.tabs)
+        val viewPager: ViewPager = findViewById(R.id.view_pager);
+        val viewPagerAdapter: ViewPagerAdapter = ViewPagerAdapter(getSupportFragmentManager())
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
+
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.madcamp_1
+package com.example.myapplication
 
 import org.junit.Test
 
