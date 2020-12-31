c2a74b9d769af6dc635a1a8220d2d1c0a0ad2d8b
jyjy1229 <kimjy18@kaist.ac.kr>
2020-12-31 15:45:41 +0900

    Show contacts at first tab which are read from json file

--- app/src/main/java/com/example/myapplication/Contact.kt
+++ app/src/main/java/com/example/myapplication/Contact.kt
@@ -1,40 +1,69 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import org.json.JSONArray
+import java.io.InputStream
+import java.lang.Exception
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Contact.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Contact : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var restaurantlist: ListView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_contact, container, false)
+        restaurantlist = view.findViewById<ListView>(R.id.restaurant_list)
+        val jsonString: String? = read_json()
+        jsonResult(jsonString)
+        return view
+    }
+
+    fun read_json(): String? {
+        var json: String? = null
+        try {
+            val inputStream: InputStream = this.requireContext().assets.open("contacts.json")
+            json = inputStream.bufferedReader().use { it.readText() }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            return null
+        }
+
+        return json
+    }
+
+    fun jsonResult(jsonString: String?) {
+        val jsonArray = JSONArray(jsonString)
+
+        val list = ArrayList<ContactData>()
+        var i = 0
+        while(i < jsonArray.length()){
+            val jsonObject = jsonArray.getJSONObject(i)
+            list.add(
+                ContactData(
+                    jsonObject.getString("name"),
+                    jsonObject.getString("phone_number"),
+                    jsonObject.getString("location")
+                )
+            )
+            i++
+        }
+        Log.d("MyApplication", list.toString())
+        val adapter = ContactAdapter(this.requireContext(), list)
+        restaurantlist.adapter = adapter
     }
 
     companion object {
