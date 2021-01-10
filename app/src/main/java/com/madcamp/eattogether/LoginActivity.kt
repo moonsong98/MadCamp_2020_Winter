3f60e7c2570dab9a40e13d1e2f65bc9703bd9d08
Moon Kyung Song <moonsont98@postech.ac.kr>
2021-01-10 18:33:11 +0900

    Fix Login Activity to Check Whether Login Session is Expired

--- app/src/main/java/com/madcamp/eattogether/LoginActivity.kt
+++ app/src/main/java/com/madcamp/eattogether/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.madcamp.eattogether
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View.VISIBLE
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -22,41 +23,33 @@ import java.util.*
 
 
 class LoginActivity: AppCompatActivity() {
-    var callbackManager = CallbackManager.Factory.create()
-    private lateinit var info : TextView
+    private val callbackManager: CallbackManager = CallbackManager.Factory.create()
     private lateinit var login : LoginButton
     private lateinit var profile: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
-        info = findViewById(R.id.info)
-        profile = findViewById(R.id.profile)
-        login = findViewById(R.id.login)
-        // Callback registration
-        login.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                // App code
-            }
 
-            override fun onCancel() {
-                // App code
-            }
-
-            override fun onError(exception: FacebookException) {
-                // App code
-            }
-        })
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-            object : FacebookCallback<LoginResult> {
+        /* Check Whether Login Session is still alive */
+        val accessToken = AccessToken.getCurrentAccessToken()
+        val isLoggedIn = accessToken != null && !accessToken.isExpired
+        /* If login session is still alive, move to next main activity */
+        if(isLoggedIn){
+            Toast.makeText(this@LoginActivity,"로그인에 성공하셨습니다.",Toast.LENGTH_SHORT).show()
+            val nextIntent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(nextIntent)
+        }
+        else {
+            profile = findViewById(R.id.profile)
+            login = findViewById(R.id.login)
+            login.visibility = VISIBLE
+            // Callback registration
+            login.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
                 override fun onSuccess(loginResult: LoginResult) {
-                    info.setText("User Id" + loginResult.accessToken.userId)
-                    var imageUrl: String =
+                    // App code
+                    val imageUrl: String =
                         "https://graph.facebook.com/" + loginResult.accessToken.userId + "/picture?return_ssl_resources=1"
                     Picasso.get().load(imageUrl).into(profile)
-//                    var memberInfo = JSONObject()
-//                    memberInfo.put("userId", loginResult.accessToken.userId.toString())
-//                    memberInfo.put("userProfileUrl", imageUrl)
                     val nextIntent = Intent(this@LoginActivity, getPhoneActivity::class.java)
                     nextIntent.putExtra("userId",loginResult.accessToken.userId.toString())
                     Log.i("aaaa",loginResult.accessToken.userId.toString())
@@ -73,33 +66,7 @@ class LoginActivity: AppCompatActivity() {
                     // App code
                 }
             })
-
-        val accessToken = AccessToken.getCurrentAccessToken()//현재 로그인되어있는지
-        val isLoggedIn = accessToken != null && !accessToken.isExpired
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
-        LoginManager.getInstance().retrieveLoginStatus(this, object: LoginStatusCallback{
-            override fun onCompleted(accessToken:AccessToken) {
-                if(isLoggedIn){
-                    Toast.makeText(this@LoginActivity,"Logged in as",Toast.LENGTH_SHORT)
-                    val nextIntent = Intent(this@LoginActivity, MainActivity::class.java)
-                    startActivity(nextIntent)
-                }
-                // User was previously logged in, can log them in directly here.
-                // If this callback is called, a popup notification appears that says
-                // "Logged in as <User Name>"
-            }
-            override fun onFailure() {
-                // No access token could be retrieved for the user
-            }
-            override fun onError(exception: Exception?) {
-                // An error occurred
-            }
-        })
-
-
-
-
-
+        }
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager.onActivityResult(requestCode, resultCode, data)
