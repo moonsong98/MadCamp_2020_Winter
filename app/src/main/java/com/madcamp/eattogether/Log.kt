80ead5277dbf886bd08631f442e6c8446d8c5071
greenina <igreen0485@gmail.com>
2021-01-10 16:17:20 +0900

    login

---
+++ app/src/main/java/com/madcamp/eattogether/Log.kt
@@ -0,0 +1,15 @@
+package com.madcamp.eattogether
+
+import android.util.Log
+
+
+object Logger {
+    fun log(tag: String?, content: String) {
+        if (content.length > 4000) {
+            Log.i(tag, content.substring(0, 4000))
+            log(tag, content.substring(4000))
+        } else {
+            Log.i(tag, content)
+        }
+    }
+}
---
+++ app/src/main/java/com/madcamp/eattogether/UserInfo.kt
@@ -0,0 +1,3 @@
+package com.madcamp.eattogether
+
+data class UserInfo(var userId: String, var name: String, var phoneNum : String)
