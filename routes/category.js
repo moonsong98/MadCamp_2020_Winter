5ba1899cdf0a8abe1e76399fe753164aaad8491b
BBang3 <hwsmin920@gmail.com>
2021-01-25 14:17:32 +0000

    Exclude irrelevant fields in response

--- routes/category.js
+++ routes/category.js
@@ -5,7 +5,7 @@ const Category = require("../models/category");
 /* GET home page. */
 router.get("/", async (req, res) => {
   try {
-    const categories = await Category.find().exec();
+    const categories = await Category.find({}, "-__v").exec();
     res.status(200).json(categories);
   } catch (error) {
     res.status(400).json({ message: "Invalid request" });
